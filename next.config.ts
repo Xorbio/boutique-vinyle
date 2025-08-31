dd05e7d181b28329f78cbbb9d1f285740e1daad1
Tom <tom.sala69340@gmail.com>
2025-08-31 12:48:56 +0200

    test pour voir image

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'https://izoqehktuhwfksrqcdbx.supabase.co',
+        hostname: 'izoqehktuhwfksrqcdbx.supabase.co',
       },
     ],
   },
