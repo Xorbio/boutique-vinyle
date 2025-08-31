01be825577876b67b7d2140c255f7ce4cc304f6a
Tom <tom.sala69340@gmail.com>
2025-08-31 12:58:15 +0200

    test pour voir image2

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'izoqehktuhwfksrqcdbx.supabase.co',
+        hostname: 'https://boutique-vinyle.vercel.app/',
       },
     ],
   },
